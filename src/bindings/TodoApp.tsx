f56089f532aa09d0bfb1ad3c01c312a9f2dc50d6
Nguyen Duc Hoang <hoangdevelopers@users.noreply.github.com>
2021-07-08 17:07:04 +0700

    Update TodoApp.tsx

--- src/bindings/TodoApp.tsx
+++ src/bindings/TodoApp.tsx
@@ -1,6 +1,11 @@
 import { connect } from 'react-redux'
 import TodoApp from '../component/TodoApp'
-const mapStateToProps = (state: string[] ) => {
+interface ITodoState {
+    todo: {
+        items: string[]
+    }
+}
+const mapStateToProps = (state: ITodoState ) => {
     return {
         todos: state.todo.items
     }
